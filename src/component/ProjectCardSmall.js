c87c5d68797eaf8959a94f05937d78ad3da762ad
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2022-02-26 20:58:17 +0100

    naprawa zębatek bo nie myślicie

--- src/component/ProjectCardSmall.js
+++ src/component/ProjectCardSmall.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Typography,
-  Box
+  Box, MenuItem, Menu
 } from "@material-ui/core";
 import SettingsIcon from '@mui/icons-material/Settings';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -9,6 +9,16 @@ import {Link} from "react-router-dom";
 
 
 function ProjectCardSmall(props) {
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{width: '100%', height: 32, background: '#4399EF', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 30, marginTop: 10}}>
     <Box sx={{width: '95%', height: 20, color: '#FFFFFF'}}>
@@ -18,10 +28,38 @@ function ProjectCardSmall(props) {
             <AddCircleOutlineIcon sx={{display: "flex", float: "right"}}/>
         }
         {props.type != "add" &&
-          <Link to={"/editproject/" + props.title} style={{color:'white'}}>
-              <SettingsIcon sx={{display: "flex", float: "right"}}/>
-          </Link>
+
+              <SettingsIcon sx={{display: "flex", float: "right"}}
+                            onClick={handleClick}
+                            size="small"
+                            aria-controls={open ? 'account-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}/>
+
         }
+
+      <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          transformOrigin={{horizontal: 'left', vertical: 'top'}}
+          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+        <MenuItem component={Link} to={"/editproject/" + props.title}>
+          Edit
+        </MenuItem>
+        <MenuItem component={Link} to={"/editproject/" + props.title}>
+          Add new member
+        </MenuItem>
+        <MenuItem component={Link} to={"/editproject/" + props.title}>
+          Add new team
+        </MenuItem>
+        <MenuItem component={Link} to={"/editproject/" + props.title}>
+          Delete
+        </MenuItem>
+      </Menu>
     </Box>
 
     </Box>
--- src/component/TeamCardSmall.js
+++ src/component/TeamCardSmall.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import {
-  Typography,
-  Box
+    Typography,
+    Box, Menu, MenuItem
 } from "@material-ui/core";
 import SettingsIcon from '@mui/icons-material/Settings';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -9,23 +9,68 @@ import {Link} from "react-router-dom";
 
 
 function TeamCardSmall(props) {
-  return (
-    <Box sx={{width: '100%', height: 32, background: '#4399EF', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 30, marginTop: 10}}>
-    <Box sx={{width: '95%', height: 20, color: '#FFFFFF'}}>
-    <Box sx={{paddingLeft: 20, height: 25, paddingTop: 5}}>
-        <Typography variant="h7" fontFamily="Sora"> {props.title} </Typography>
-        {props.type == "add" &&
-            <AddCircleOutlineIcon sx={{display: "flex", float: "right"}}/>
-        }
-        {props.type != "add" &&
-        <Link to={"/editteam/" + props.title} style={{color:'white'}}>
-            <SettingsIcon sx={{display: "flex", float: "right"}}/>
-        </Link>
-        }
-    </Box>
 
-    </Box>
-    </Box>
-  );
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <Box sx={{
+            width: '100%',
+            height: 32,
+            background: '#4399EF',
+            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+            borderRadius: 30,
+            marginTop: 10
+        }}>
+            <Box sx={{width: '95%', height: 20, color: '#FFFFFF'}}>
+                <Box sx={{paddingLeft: 20, height: 25, paddingTop: 5}}>
+                    <Typography variant="h7" fontFamily="Sora"> {props.title} </Typography>
+                    {props.type == "add" &&
+                        <AddCircleOutlineIcon sx={{display: "flex", float: "right"}}/>
+
+                    }
+
+                    {props.type !== "add" &&
+                        <SettingsIcon
+                            sx={{display: "flex", float: "right", marginRight: "5px"}}
+                            onClick={handleClick}
+                            size="small"
+                            aria-controls={open ? 'account-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                        />
+                    }
+
+                    <Menu
+                        anchorEl={anchorEl}
+                        id="account-menu"
+                        open={open}
+                        onClose={handleClose}
+                        onClick={handleClose}
+                        transformOrigin={{horizontal: 'left', vertical: 'top'}}
+                        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+                    >
+                            <MenuItem component={Link} to={"/editproject/" + props.title}>
+                            Edit
+                            </MenuItem>
+                            <MenuItem component={Link} to={"/editproject/" + props.title}>
+                                Add new member
+                            </MenuItem>
+                            <MenuItem component={Link} to={"/editproject/" + props.title}>
+                                Delete
+                            </MenuItem>
+                    </Menu>
+
+
+                </Box>
+
+            </Box>
+        </Box>
+);
 }
 export default TeamCardSmall;
