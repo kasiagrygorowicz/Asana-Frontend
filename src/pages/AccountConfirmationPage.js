8eb9fdd86678ef243aa2c16990882ae2eb02e58a
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2022-03-17 12:21:22 +0100

    animacja teczy

--- src/pages/AccountConfirmationPage.js
+++ src/pages/AccountConfirmationPage.js
@@ -1,13 +1,35 @@
 import {Link} from "react-router-dom";
 import React from "react";
+import './css/rainbow.css'
+
+
 
 const AccountConfirmationPage =(props,response)=>{
+
+
+
     return(
     <div>
         <p>Your account has been activated</p>
         <Link to="/login" >
             {props.t('login')}
         </Link>
+        <div className={"center"}>
+            <ul >
+                <li ></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+
+
+            </ul>
+
+        </div>
+
+
 
     </div>)
 
