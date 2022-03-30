e188862ca4d13c385188047e1e18a9587d91fcf0
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2022-03-30 23:49:57 +0200

    dodanie ikoenk zegara

--- src/component/TasksColumn.js
+++ src/component/TasksColumn.js
@@ -1,8 +1,48 @@
-import {Box, Grid, Typography} from "@material-ui/core";
+import {Box, Grid, Popover, Typography} from "@material-ui/core";
 import {Droppable} from "react-beautiful-dnd";
 import Stack from "@mui/material/Stack";
 import React, {useState} from "react";
 import Task from "./Task";
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import {AccessTime} from "@mui/icons-material";
+
+
+function TimeIconPopover() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+    return (
+        <div >
+            <AccessTime onClick={handleClick}/>
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+            >
+                <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+            </Popover>
+        </div>
+    );
+}
+
+
+
+
 
 const TasksColumn = ({t, tasks, type}) => {
     return (
@@ -10,18 +50,30 @@ const TasksColumn = ({t, tasks, type}) => {
             <Box sx={{
                 width: '100%',
                 height: 80,
-                alignItems: 'center',
-                display: 'flex',
                 float: 'left',
                 borderRight: type !== 'DONE' ? '1px solid black' : 'none',
-                borderBottom: '1px solid black'
+                borderBottom: '1px solid black',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems :'center',
+                justifyContent :'center'
+
+                // bgcolor:'blue'
             }}>
+
+
                 <Typography variant="h4" fontFamily="Sora"
                             style={{
                                 fontWeight: 'bold',
                                 textAlign: 'center',
-                                width: '80%'
-                            }}>{t(type.toLowerCase())}</Typography>
+                                // width: '60%',
+                                marginRight:'5px'
+                            }}>{t(type.toLowerCase())}
+                </Typography>
+                <TimeIconPopover/>
+
+
+
             </Box>
             <Box sx={{
                 width: '100%',
