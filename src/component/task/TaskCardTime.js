6f3320b5c70e4c1d492ccf49663dd3f5bf257978
Marek Nowakowski <marsznow@gmail.com>
2022-04-06 00:18:30 +0200

    Czas na projekt - pobieranie danych

--- src/component/task/TaskCardTime.js
+++ src/component/task/TaskCardTime.js
@@ -15,12 +15,12 @@ function TaskCardTime(props) {
     const hours = <span>{("0" + Math.floor(props.time / 3600)).slice(-2)}</span>;
 
     return (
-        <Box sx={{width: '22%', height: 100, background: props.cardColor, boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 30}}>
+        <Box sx={{width: '30%', height: 100, background: props.cardColor, boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 30}}>
         <Box sx={{width: '50%', height: 80, color: '#FFFFFF'}}>
         <Stack direction="column">
             <Stack direction="row">
-                <Box sx={{paddingLeft: 30, height: 35, paddingTop: 7.5, width: '75%'}}>
-                <Typography variant="h5" fontFamily="Sora" noWrap="true" style={{fontWeight: 'bold', overflow: 'hidden' }}> {props.taskName} </Typography>
+                <Box sx={{paddingLeft: 30, height: 35, paddingTop: 7.5, width: '150%'}}>
+                <Typography variant="h6" fontFamily="Sora" noWrap="true" style={{fontWeight: 'bold', overflow: 'hidden' }}> {props.taskName} </Typography>
                 </Box>
             </Stack>
                 <Box sx={{paddingLeft: 30, height: 0, width: '90%'}}>
--- src/pages/TimeManagement.js
+++ src/pages/TimeManagement.js
@@ -1,24 +1,28 @@
 import Container from "@mui/material/Container";
 import {Box, Typography} from "@mui/material";
-import React from "react";
-import Stack from "@mui/material/Stack";
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import React, {useEffect, useState} from 'react'
 import TaskCardTime from "../component/task/TaskCardTime";
+import useUserProjects from "../hook/use-projects";
+import useFetch from "../hook/use-fetch";
 
 const TimeManagement = () =>{
-    return(
-        <Container maxWidth={false} sx={{
-            display: "flex",
-            flexDirection: "column",
-            padding: "50px",
-            marginLeft: "17%",
-            width: "80%",
-        }}>
-            {/*<Box width="100%" height="20px" borderBottom="1px solid black">*/}
-            <Typography color="text.secondary" variant="h3" sx={{
-               alignSelf: "flex-start"
-            }}>My Projects Times</Typography>
-            {/*</Box>*/}
+    const { isProjectLoading, projectError, sendRequest: fetchProject } = useFetch();
+    let [ projectsInfo, setProjectsInfo ] = useState([]);
+
+    useEffect(() => {
+            const handleProject = (response) => {
+                setProjectsInfo(response);
+            }
+    
+            const fetchProjectRequest = {
+                url: `/project/all/time`
+            }
+    
+            fetchProject(fetchProjectRequest, handleProject);
+    }, [fetchProject])
+
+    let seconds, minutes, hours;
+    let projectsToDisplay = projectsInfo.map((project) => (
             <Box sx={{
                 border: "1px solid #ADB5BD",
                 boxSizing: "border-box",
@@ -53,8 +57,12 @@ const TimeManagement = () =>{
                         display: "flex",
                         flexDirection: "column"
                     }}>
-                        <Typography sx={{ color: "#2196f3", fontSize: "36px" }}>Project A</Typography>
-                        <Typography sx={{ color: "#2196f3", fontSize: "16px"}}>69h : 21m : 37s</Typography>
+                        <Typography sx={{ color: "#2196f3", fontSize: "36px" }}>{project.projectName}</Typography>
+                        <Typography sx={{ color: "#2196f3", fontSize: "16px"}}>
+                            <span>{("0" + Math.floor(project.totalTimeOnProject / 3600)).slice(-2)}</span>h : 
+                            <span>{("0" + Math.floor(project.totalTimeOnProject / 60 % 60)).slice(-2)}</span>m : 
+                            <span>{("0" + Math.floor(project.totalTimeOnProject % 60)).slice(-2)}</span>s
+                        </Typography>
                     </Box>
                 </Box>
                 <Box sx={{
@@ -66,215 +74,35 @@ const TimeManagement = () =>{
                     marginTop: "15px",
                     padding: "15px 0"
                 }}>
-                    <TaskCardTime
-                        taskName="task1"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={2154}
-                    />
-
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
+                    {
+                        project.timeOnTaskList.map((task) => (
+                            <TaskCardTime
+                            taskName={task.taskName}
+                            taskType={task.taskStatus}
+                            cardColor="#4F6C89"
+                            time={task.timeSpent}
+                            />
+                        ))
+                    }
                 </Box>
             </Box>
-            <Box sx={{
-                border: "1px solid #ADB5BD",
-                boxSizing: "border-box",
-                boxShadow: "2px 3px 5px rgba(0, 0, 0, 0.25)",
-                borderRadius: "20px",
-                padding: "30px",
-                paddingTop: "10px",
-                marginTop: "30px",
-                width: "60%"
-            }}>
-                <Box sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: 'flex-start',
-                    backgroundColor: "#f5f5f5",
-                    borderRadius: "10px",
-                    padding: "5px 10px",
-                    gap: 3
-                }}>
-                    <Box sx={{
-                        display: "flex",
-                        flexDirection: "column"
-                    }}>
-                        <Typography sx={{
-                            fontSize: "36px",
-                            color: "#495057"
-                        }}>Project: </Typography>
-                        <Typography sx={{fontSize: "16px"}}>Total time spent:</Typography>
-                    </Box>
-                    <Box sx={{
-                        display: "flex",
-                        flexDirection: "column"
-                    }}>
-                        <Typography sx={{ color: "#2196f3", fontSize: "36px" }}>Project A</Typography>
-                        <Typography sx={{ color: "#2196f3", fontSize: "16px"}}>69h : 21m : 37s</Typography>
-                    </Box>
-                </Box>
-                <Box sx={{
-                    display: "flex",
-                    justifyContent: 'flex-start',
-                    gap: 4,
-                    flexWrap: "wrap",
-                    borderTop: "1px solid #DEE2E6",
-                    marginTop: "15px",
-                    padding: "15px 0"
-                }}>
-                    <TaskCardTime
-                        taskName="task1"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={2154}
-                    />
+    ));
 
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                </Box>
-            </Box>
-            <Box sx={{
-                border: "1px solid #ADB5BD",
-                boxSizing: "border-box",
-                boxShadow: "2px 3px 5px rgba(0, 0, 0, 0.25)",
-                borderRadius: "20px",
-                padding: "30px",
-                paddingTop: "10px",
-                marginTop: "30px",
-                width: "60%"
-            }}>
-                <Box sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: 'flex-start',
-                    backgroundColor: "#f5f5f5",
-                    borderRadius: "10px",
-                    padding: "5px 10px",
-                    gap: 3
-                }}>
-                    <Box sx={{
-                        display: "flex",
-                        flexDirection: "column"
-                    }}>
-                        <Typography sx={{
-                            fontSize: "36px",
-                            color: "#495057"
-                        }}>Project: </Typography>
-                        <Typography sx={{fontSize: "16px"}}>Total time spent:</Typography>
-                    </Box>
-                    <Box sx={{
-                        display: "flex",
-                        flexDirection: "column"
-                    }}>
-                        <Typography sx={{ color: "#2196f3", fontSize: "36px" }}>Project A</Typography>
-                        <Typography sx={{ color: "#2196f3", fontSize: "16px"}}>69h : 21m : 37s</Typography>
-                    </Box>
-                </Box>
-                <Box sx={{
-                    display: "flex",
-                    justifyContent: 'flex-start',
-                    gap: 4,
-                    flexWrap: "wrap",
-                    borderTop: "1px solid #DEE2E6",
-                    marginTop: "15px",
-                    padding: "15px 0"
-                }}>
-                    <TaskCardTime
-                        taskName="task1"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={2154}
-                    />
-
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                    <TaskCardTime
-                        taskName="task2"
-                        taskType="done"
-                        cardColor="#4F6C89"
-                        time={245}
-                    />
-                </Box>
-            </Box>
+    return(
+        <Container maxWidth={false} sx={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "50px",
+            marginLeft: "17%",
+            width: "80%",
+        }}>
+            {/*<Box width="100%" height="20px" borderBottom="1px solid black">*/}
+            <Typography color="text.secondary" variant="h3" sx={{
+               alignSelf: "flex-start"
+            }}>My Projects Times</Typography>
+            {/*</Box>*/}
+            {projectsToDisplay}
+            
         </Container>
     );
 }
